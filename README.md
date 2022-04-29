b2f242f1bcf37008722fa8049f98f69a7a5adfdb
John Cassidy <jocassid@gmail.com>
2022-04-29 00:32:41 -0400

    Added function to compile SASS to CSS

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # homepage
 Code to generate page with links to commonly used sites
+
+## Requirements
+- Python (3.6 or greater)
+- node
+- make
--- homepage.py
+++ homepage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+from os import system
+from os.path import exists
 from pathlib import Path
+from stat import ST_MTIME
 from sys import exit, stderr
 
 from jinja2 import Environment, select_autoescape, FileSystemLoader
@@ -22,8 +25,11 @@ def parse_contents(contents):
         yield item
 
 
-def main(args):
-    print(args)
+def mod_time(path):
+    return path.stat()[ST_MTIME]
+
+
+def render_html(input_file, output_dir):
 
     env = Environment(
         loader=FileSystemLoader(
@@ -32,32 +38,62 @@ def main(args):
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    output_dir_path = Path(args.output_dir)
-    if not output_dir_path.exists():
-        print(f"{args.output_dir} doesn't exist", file=stderr)
+    if not exists(input_file):
+        print(f"{input_file} doesn't exist", file=stderr)
+        return False
+
+    with open(input_file, 'rb') as input_file:
+        data = load(input_file, Loader=Loader)
+        # print(f"data is {data}")
+
+        for page in data.get('pages', []):
+            template = page.get('template')
+            template = env.get_template(template)
+
+            file_name = page.get('file')
+            if not file_name:
+                continue
+
+            output_file = output_dir / file_name
+            with open(str(output_file), 'w') as out_file:
+                out_file.write(
+                    template.render(
+                        contents=parse_contents(
+                            page.get('contents', [])
+                        ),
+                    )
+                )
+    return True
+
+
+def generate_css(homepage_dir, output_dir):
+    source_file = homepage_dir / 'style.scss'
+    dest_file = output_dir / 'style.css'
+
+    if not source_file.exists():
+        print(f"{source_file} not found", file=stderr)
+        return
+
+    if dest_file.exists() and mod_time(dest_file) >= mod_time(source_file):
+        # destination exist and is newer than source
+        return True
+
+    sass_path = homepage_dir / 'node_modules/sass/sass.js'
+    command = f"node {sass_path} {source_file} {dest_file}"
+    return system(command) == 0
+
+
+def main(args):
+
+    output_dir = Path(args.output_dir)
+    if not output_dir.exists():
+        print(f"{output_dir} doesn't exist", file=stderr)
         exit(1)
 
-    with open(args.input_file, 'rb') as in_file:
-        data = load(in_file, Loader=Loader)
-        print(f"data is {data}")
+    homepage_dir = Path(__file__).parent
 
-    for page in data.get('pages', []):
-        template = page.get('template')
-        template = env.get_template(template)
-
-        file_name = page.get('file')
-        if not file_name:
-            continue
-
-        out_file_path = output_dir_path / file_name
-        with open(str(out_file_path), 'w') as out_file:
-            out_file.write(
-                template.render(
-                    contents=parse_contents(
-                        page.get('contents', [])
-                    ),
-                )
-            )
+    render_html(args.input_file, output_dir)
+    generate_css(homepage_dir, output_dir)
 
 
 if __name__ == '__main__':
